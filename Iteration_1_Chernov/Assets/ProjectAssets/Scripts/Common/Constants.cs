ab9a52a83299be32f3ec34a61d340741da064a4e
Oleksii Chernov <aastudio15@gmail.com>
2017-09-21 08:05:12 +0300

    add unstaged files

---
+++ Iteration_1_Chernov/Assets/ProjectAssets/Scripts/Common/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iteration1.Common
+{
+    public class Constants
+    {
+        public class SceneNames
+        {
+            public const string LodSceneName = "LOD";
+            public const string BatchingSceneName = "Batching";
+            public const string CullingSceneName = "Culling";
+            public const string NavigationSceneName = "Navigation";
+            public const string LightmapsSceneName = "Lightmaps";
+            public const string AnimationsSceneName = "Aniamtions";
+            public const string LightprobeSceneName = "Lights";
+        }
+    }
+}
---
+++ Iteration_1_Chernov/Assets/ProjectAssets/Scripts/Common/MainMenuController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Iteration1.Common
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _mainUi;
+
+        public void Start()
+        {
+            _mainUi.SetActive(true);
+        }
+
+        public void OnDisable()
+        {
+           _mainUi.SetActive(false); 
+        }
+
+        public void LodButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.LodSceneName, LoadSceneMode.Additive);
+        }
+
+        public void BatchingButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.BatchingSceneName, LoadSceneMode.Additive);
+        }
+
+        public void CullingButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.CullingSceneName, LoadSceneMode.Additive);
+        }
+
+        public void NavigationButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.NavigationSceneName, LoadSceneMode.Additive);
+        }
+
+        public void LightmapsButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.LightmapsSceneName, LoadSceneMode.Additive);
+        }
+
+        public void AnimationsButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.AnimationsSceneName, LoadSceneMode.Additive);
+        }
+
+        public void LightprobeButtonHandler()
+        {
+            SceneManager.LoadScene(Constants.SceneNames.LightprobeSceneName, LoadSceneMode.Additive);
+        }
+    }
+}
